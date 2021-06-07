7984ba52bad372f097750512ca57532837edbdf6
Amaury0602 <amauryropert@gmail.com>
2021-06-07 18:13:34 +0200

    Changed camera settings
    Added Video and Game Mode to switch resolutions easily;
    + automatic ortho size change

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,11 +8,11 @@ public class CameraController : MonoBehaviour
     private Bounds targetBounds;
     private float targetRatio;
 
-    private float minCamDistance = 12.5f;
-    private float maxCamDistance = 17.5f;
+    [SerializeField] private CamMode mode;
 
     private void Awake()
     {
+        mode = CamMode.Game;
         cam = GetComponent<Camera>();
         startYPos = transform.position.y;
     }
@@ -30,28 +30,21 @@ public class CameraController : MonoBehaviour
     public void SetCameraPositionAndSize(Bounds levelBounds)
     {
         Vector3 center = levelBounds.center;
-        LeanTween.move(gameObject, new Vector3(center.x + 0.5f, startYPos, center.z), 0.5f);
+        LeanTween.move(gameObject, new Vector3(center.x, startYPos, center.z), 0.5f);
 
-        float boundsHeight = levelBounds.size.y;
-        float boundsWidth = levelBounds.size.x;
-
-        float camDistance;
-
-        // y = y1 + ((x – x1) / (x2 – x1)) * (y2 – y1)
-        // camHeight = minCam + ((14-10) / ()) * (17,5 - 12,5)
-
-        if (boundsWidth <= 10)
+        float boundsDiag = Mathf.Sqrt(Mathf.Pow(levelBounds.size.y, 2) + Mathf.Pow(levelBounds.size.x, 2));
+        float newOrthSize = mode == CamMode.Game ? boundsDiag * 0.95f : boundsDiag * 0.7f;
+        if (mode == CamMode.Game)
         {
-            camDistance = minCamDistance;
-        }
-        else if (boundsWidth >= 14.5f)
-        {
-            camDistance = maxCamDistance;
+            cam.DOOrthoSize(Mathf.Clamp(newOrthSize, 12.5f, 17.5f), 0.2f);
         } else
         {
-            camDistance = 15f;
-            //camDistance = minCamDistance + ((14-10) / (boundsWidth - 10)) * (maxCamDistance - minCamDistance);
+            cam.DOOrthoSize(Mathf.Clamp(newOrthSize, 10f, 12f), 0.2f);
         }
-        cam.DOOrthoSize(camDistance, 0.5f);
+    }
+
+    private enum CamMode
+    {
+        Video, Game
     }
 }
